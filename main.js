0df92af1f4b8ccd741652adde4c3dae1eb80c3b9
Ben Tonks <thebentonks@gmail.com>
2019-08-03 11:29:48 +0100

    Modified+added to all files

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
-function getData(cb) {
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
     var xhr = new XMLHttpRequest();
-    
-    xhr.open("GET", "https://swapi.co/api/")    
-    xhr.send();
-    
-    xhr.onreadystatechange = function () {
+
+    xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-function printDataToConsole(data) {
-    console.log(data);
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
-getData(printDataToConsole);
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
 
+    getData(type, function(data) {
+        var tableRows = [];
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
+        data.forEach(function(item) {
+            var dataRow = [];
 
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow)
+        });
 
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
