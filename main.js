4684743629304290f069d7772b8398a29bb29e33
Ben Tonks <thebentonks@gmail.com>
2019-08-04 11:22:05 +0100

    truncated data

--- main.js
+++ main.js
@@ -36,11 +36,13 @@ function writeToDocument(type) {
             var dataRow = [];
 
             Object.keys(item).forEach(function(key) {
-                dataRow.push(`<td>${item[key]}</td>`);
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
-            tableRows.push(dataRow)
+            tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
